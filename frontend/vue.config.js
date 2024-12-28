0735b06f2f8d350b8ba0d52d77e546f90595a247
shubkb07 <shub@shubkb.com>
2024-12-29 00:13:17 +0530

    set output directory to 'dist' in vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: 'dist', // Ensure the output directory is set to 'dist'
   devServer: {
     proxy: {
       '/api': {
